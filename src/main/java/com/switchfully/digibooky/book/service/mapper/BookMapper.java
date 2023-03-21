fb405db50a5401ba4c8c038321a6a529103da832
Yves Manzi <yv.manzi@gmail.com>
2023-03-21 17:14:52 +0100

    [ADD] init and finish BookMapper

---
+++ src/main/java/com/switchfully/digibooky/book/service/mapper/BookMapper.java
@@ -0,0 +1,26 @@
+package com.switchfully.digibooky.book.service.mapper;
+
+import com.switchfully.digibooky.book.domain.Book;
+import com.switchfully.digibooky.book.service.dto.BookDto;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+
+@Component
+public class BookMapper {
+
+    public BookDto toDto(Book book){
+        return new BookDto(book.getIsbn(), book.getTitle(), book.getAuthor(), book.getSummary());
+    }
+
+    public List<BookDto> toDto(List<Book> listOfBooks){
+        return listOfBooks
+                .stream()
+                .map(this::toDto)
+                .toList();
+    }
+
+    public Book fromDto(BookDto bookDto){
+        return new Book(bookDto.getIsbn(), bookDto.getTitle(), bookDto.getAuthor(), bookDto.getSummary());
+    }
+}
