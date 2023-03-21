9525204b4d0b49bf85362b44ac6a15f9cdbb80fc
Yves Manzi <yv.manzi@gmail.com>
2023-03-21 17:08:34 +0100

    [ADD] init and finish Author Class

---
+++ src/main/java/com/switchfully/digibooky/book/domain/Author.java
@@ -0,0 +1,42 @@
+package com.switchfully.digibooky.book.domain;
+
+import java.util.Objects;
+
+public class Author {
+    private final String firstname;
+    private final String lastname;
+
+    public Author(String firstname, String lastname) {
+        this.firstname = firstname;
+        this.lastname = lastname;
+    }
+
+    public String getFirstname() {
+        return firstname;
+    }
+
+    public String getLastname() {
+        return lastname;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Author author = (Author) o;
+        return Objects.equals(firstname, author.firstname) && Objects.equals(lastname, author.lastname);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(firstname, lastname);
+    }
+
+    @Override
+    public String toString() {
+        return "Author{" +
+                "firstname='" + firstname + '\'' +
+                ", lastname='" + lastname + '\'' +
+                '}';
+    }
+}
