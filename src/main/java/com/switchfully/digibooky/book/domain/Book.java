3f8365d2326e4b6d6300a778fda4f2c0e02d0940
Yves Manzi <yv.manzi@gmail.com>
2023-03-21 17:11:01 +0100

    [ADD] init and finish Book Class without isDeleted

--- src/main/java/com/switchfully/digibooky/book/domain/Book.java
+++ src/main/java/com/switchfully/digibooky/book/domain/Book.java
@@ -1,4 +1,69 @@
 package com.switchfully.digibooky.book.domain;
 
+import java.util.Objects;
+
 public class Book {
+    private final String isbn;
+    private String title;
+
+    private Author author;
+    private String summary;
+
+    public Book(String isbn, String title, Author author, String summary) {
+        this.isbn = isbn;
+        this.title = title;
+        this.author = author;
+        this.summary = summary;
+    }
+
+    public String getIsbn() {
+        return isbn;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public Author getAuthor() {
+        return author;
+    }
+
+    public String getSummary() {
+        return summary;
+    }
+
+    public void setSummary(String summary) {
+        this.summary = summary;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public void setAuthor(Author author) {
+        this.author = author;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Book book = (Book) o;
+        return Objects.equals(isbn, book.isbn);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(isbn);
+    }
+
+    @Override
+    public String toString() {
+        return "Book{" +
+                "isbn='" + isbn + '\'' +
+                ", title='" + title + '\'' +
+                ", author=" + author +
+                ", summary='" + summary + '\'' +
+                '}';
+    }
 }
