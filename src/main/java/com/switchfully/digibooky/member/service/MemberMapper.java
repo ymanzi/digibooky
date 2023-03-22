68ba6e75313fe6c2cc10a7bb7ad80a74b7ed023b
TournayGilles <Tournay.gilles.94@gmail.com>
2023-03-22 14:28:02 +0100

    [ADD] Member mapper

--- src/main/java/com/switchfully/digibooky/member/service/MemberMapper.java
+++ src/main/java/com/switchfully/digibooky/member/service/MemberMapper.java
@@ -1,4 +1,25 @@
 package com.switchfully.digibooky.member.service;
 
+import com.switchfully.digibooky.member.domain.Member;
+import java.util.Collection;
+import java.util.List;
+
 public class MemberMapper {
+    public MemberDto toDto(Member member){
+        return new MemberDto(member.getId(), member.getFirstname(), member.getLastName(), member.getEmail(), member.getAddress(), member.getRole());
+    }
+    public Member fromDto(CreateMemberDto memberDto){
+        Member member = new Member.MemberBuilder()
+                .withINSS(memberDto.getINSS())
+                .withAddress(memberDto.getAddress())
+                .withEmail(memberDto.getEmail())
+                .withFirstname(memberDto.getFirstname())
+                .withLastName(memberDto.getLastName())
+                .withRole(memberDto.getRole())
+                .build();
+        return member;
+    }
+    public List<MemberDto> memberListToDto(Collection<Member> members){
+        return members.stream().map(member -> toDto(member)).toList();
+    }
 }
