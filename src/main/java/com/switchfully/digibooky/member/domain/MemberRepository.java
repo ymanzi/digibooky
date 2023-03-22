82cc49280b26c67b629ca7d5724f5f9374e4f66a
TournayGilles <Tournay.gilles.94@gmail.com>
2023-03-22 14:27:45 +0100

    [ADD] Member repository

--- src/main/java/com/switchfully/digibooky/member/domain/MemberRepository.java
+++ src/main/java/com/switchfully/digibooky/member/domain/MemberRepository.java
@@ -1,4 +1,57 @@
 package com.switchfully.digibooky.member.domain;
 
+import org.springframework.stereotype.Repository;
+
+import java.util.Collection;
+import java.util.Map;
+import java.util.UUID;
+import java.util.concurrent.ConcurrentHashMap;
+
+@Repository
 public class MemberRepository {
+    private final Map<UUID, Member> memberByUUIDRepository;
+
+    public MemberRepository() {
+        memberByUUIDRepository = new ConcurrentHashMap<>();
+        Member admin = new Member.MemberBuilder()
+                .withEmail("admin@admin.com")
+                .withAddress(new Address("a", "a", "a", "a"))
+                .withFirstname("admin")
+                .withLastName("admin")
+                .withINSS("000-000-000")
+                .withRole(Role.ADMIN)
+                .build();
+        memberByUUIDRepository.put(admin.getId(), admin);
+    }
+    public Member save(Member newMember){
+        checkIfUniqueINSS(newMember);
+        checkIfUniqueEmail(newMember);
+        return memberByUUIDRepository.put(newMember.getId(),newMember);
+    }
+
+    private void checkIfUniqueEmail(Member newMember) {
+        Member validateNotPresent= memberByUUIDRepository.values()
+                .stream()
+                .filter(member -> member.getEmail().equals(newMember.getEmail()))
+                .findFirst()
+                .orElse(null);
+        if (validateNotPresent != null)
+            throw new RuntimeException("placeholder");
+    }
+
+    private void checkIfUniqueINSS (Member newMember) {
+        Member validateNotPresent = memberByUUIDRepository.values()
+                .stream()
+                .filter(member -> member.getINSS().equals(newMember.getINSS()))
+                .findFirst()
+                .orElse(null);
+        if (validateNotPresent != null)
+            throw new RuntimeException("placeholder");
+    }
+    public Collection<Member> getAllMembers(){
+        return memberByUUIDRepository.values();
+    }
+    public Member getMemberById(UUID id){
+        return memberByUUIDRepository.get(id);
+    }
 }
