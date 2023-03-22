70406ce363f26794c6ab7facfc19e2866fe1fc85
henrigvs <henri.gevenois@gmail.com>
2023-03-22 16:35:00 +0100

    [ADD] MemberDtoTest

---
+++ src/test/java/com/switchfully/digibooky/member/service/dtos/MemberDtoTest.java
@@ -0,0 +1,27 @@
+package com.switchfully.digibooky.member.service.dtos;
+
+import com.switchfully.digibooky.member.domain.Address;
+import com.switchfully.digibooky.member.domain.Role;
+import com.switchfully.digibooky.member.service.MemberDto;
+import org.junit.jupiter.api.Test;
+
+import java.util.UUID;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class MemberDtoTest {
+
+    @Test
+    public void testConstructorAndGetters() {
+        UUID id = UUID.randomUUID();
+        Address address = new Address("Grand Place", "1", "1000", "Bruxelles");
+        MemberDto dto = new MemberDto(id, "Georges", "Remi", "tintinmilou@herge.be", address, Role.MEMBER);
+
+        assertEquals(id, dto.getId());
+        assertEquals("Georges", dto.getFirstname());
+        assertEquals("Remi", dto.getLastName());
+        assertEquals("tintinmilou@herge.be", dto.getEmail());
+        assertEquals(address, dto.getAddress());
+        assertEquals(Role.MEMBER, dto.getRole());
+    }
+}
