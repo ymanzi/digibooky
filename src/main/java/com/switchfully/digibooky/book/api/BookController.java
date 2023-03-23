cd5720a4bce7a28c093025003f5534eaf8b5c805
Esteban Veraart <esteban.veraart@gmail.com>
2023-03-23 17:56:37 +0100

    [REF] Disposition and general refactor [ADD] @DeleteMapping & @PutMapping

--- src/main/java/com/switchfully/digibooky/book/api/BookController.java
+++ src/main/java/com/switchfully/digibooky/book/api/BookController.java
@@ -11,6 +11,7 @@ import java.util.List;
 @RestController
 @RequestMapping(path = "/books")
 public class BookController {
+
     private final BookService bookService;
 
     @Autowired
@@ -18,11 +19,14 @@ public class BookController {
         this.bookService = bookService;
     }
 
+    //============= GET =================================================================
+
     @GetMapping(produces = "application/json")
     @ResponseStatus(HttpStatus.OK)
     public List<BookDto> getAll(){
         return bookService.getAll();
     }
+
 
     @GetMapping(path = "/isbn/{isbn}", produces = "application/json")
     @ResponseStatus(HttpStatus.OK)
@@ -30,16 +34,49 @@ public class BookController {
         return bookService.getByIsbn(isbn);
     }
 
-    @GetMapping(path = "/author", consumes = "application/json",produces = "application/json")
+
+    @GetMapping(path = "/author/{authorId}", consumes = "application/json",produces = "application/json")
     @ResponseStatus(HttpStatus.OK)
-    public List<BookDto> getByAuthor(@RequestBody AuthorDto authorDto){
-        return bookService.getByAuthor(authorDto);
+    public List<BookDto> getByAuthor(@PathVariable String authorId){
+        return bookService.getByAuthor(authorId);
     }
+
 
     @GetMapping(path = "/title/{title}", produces = "application/json")
     @ResponseStatus(HttpStatus.OK)
     public List<BookDto> getByTitle(@PathVariable String title){
         return bookService.getByTitle(title);
+    }
+
+
+    //============= POST =================================================================================
+
+    @PostMapping(path = "", consumes = "application/json",produces = "application/json")
+    @ResponseStatus(HttpStatus.CREATED)
+    public BookDto create(@RequestBody BookDto bookDto, @RequestHeader String id){
+        return bookService.save(bookDto, id);
+    }
+
+    //============= PUT ==================================================================================
+
+    @PutMapping(consumes="application/json", produces="application/json")
+    @ResponseStatus(HttpStatus.OK)
+    public BookDto updateOneBook(@RequestBody BookDto updateBookDto, @RequestHeader String id){
+        return bookService.update(updateBookDto, id);
+    }
+
+    //============= DELETE ==============================================================================
+
+    @DeleteMapping(path = "/isbn/{isbn}", produces = "application/json")
+    @ResponseStatus(HttpStatus.OK)
+    public List<BookDto> deleteByIsbn(@PathVariable String isbn, @RequestHeader String id){
+        return bookService.delete(isbn, id, "isbn");
+    }
+
+    @DeleteMapping(path = "/title/{title}", produces = "application/json")
+    @ResponseStatus(HttpStatus.OK)
+    public List<BookDto> deleteByTitle(@PathVariable String title, @RequestHeader String id){
+        return bookService.delete(title, id, "title");
     }
 
     @PostMapping(path = "", consumes = "application/json",produces = "application/json")
