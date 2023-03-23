4000eacb7d769b3db8b0d49f615d6958b01b7a9b
Esteban Veraart <esteban.veraart@gmail.com>
2023-03-23 17:45:58 +0100

    [ADD] init and finish BookTest

---
+++ src/test/java/com/switchfully/digibooky/book/domain/BookTest.java
@@ -0,0 +1,46 @@
+package com.switchfully.digibooky.book.domain;
+
+import com.switchfully.digibooky.book.exceptions.NoTitleForBookException;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+public class BookTest {
+
+    @Test
+    void createBookWithEmptyTitle_thenRaiseNoTitleForBookException() {
+        //Then
+        assertThrows(NoTitleForBookException.class, () -> new Book("isbn", "", new Author("first", "last"), "summary"));
+    }
+
+    @Test
+    void createBookWithBlankLastname_thenRaiseNoTitleForBookException() {
+        //Then
+        assertThrows(NoTitleForBookException.class, () -> new Book("isbn", "    ", new Author("first", "last"), "summary"));
+    }
+
+    @Test
+    void createBookWithNullLastname_thenRaiseNoTitleForBookException() {
+        //Then
+        assertThrows(NoTitleForBookException.class, () -> new Book("isbn", null, new Author("first", "last"), "summary"));
+    }
+
+    @Test
+    void createBookWithEmptyAuthorLastname_thenRaiseNoTitleForBookException() {
+        //Then
+        assertThrows(NoTitleForBookException.class, () -> new Book("isbn", "title", new Author("first", ""), "summary"));
+    }
+
+    @Test
+    void createBookWithBlankAuthorLastname_thenRaiseNoTitleForBookException() {
+        //Then
+        assertThrows(NoTitleForBookException.class, () -> new Book("isbn", "title", new Author("first", "     "), "summary"));
+    }
+
+    @Test
+    void createBookWithNullAuthorLastname_thenRaiseNoTitleForBookException() {
+        //Then
+        assertThrows(NoTitleForBookException.class, () -> new Book("isbn", "title", new Author("first", null), "summary"));
+    }
+
+}
