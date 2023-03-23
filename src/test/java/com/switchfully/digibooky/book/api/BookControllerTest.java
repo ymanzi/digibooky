458d6263a990080a83f92848dab5c4dc3c23f1ec
Mrouve <maximerouve@hotmail.com>
2023-03-23 10:22:56 +0100

    [IMP] Test - modification to empty context

--- src/test/java/com/switchfully/digibooky/book/api/BookControllerTest.java
+++ src/test/java/com/switchfully/digibooky/book/api/BookControllerTest.java
@@ -8,14 +8,17 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.DirtiesContext;
 
 import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
+@DirtiesContext(classMode = DirtiesContext.ClassMode.AFTER_EACH_TEST_METHOD)
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.DEFINED_PORT)
 class BookControllerTest {
 
+    
     @Autowired
     private BookController bookController;
 
