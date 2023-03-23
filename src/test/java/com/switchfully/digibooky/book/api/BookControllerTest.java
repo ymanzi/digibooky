d3a3b4d409544e841eb7f3758bc6f9e63ede68df
Esteban Veraart <esteban.veraart@gmail.com>
2023-03-23 17:37:49 +0100

    [REF] testes refactored [ADD] test finish

--- src/test/java/com/switchfully/digibooky/book/api/BookControllerTest.java
+++ src/test/java/com/switchfully/digibooky/book/api/BookControllerTest.java
@@ -1,8 +1,13 @@
 package com.switchfully.digibooky.book.api;
 
 import com.switchfully.digibooky.book.domain.Author;
-import com.switchfully.digibooky.book.service.dto.AuthorDto;
+import com.switchfully.digibooky.book.domain.Book;
+import com.switchfully.digibooky.book.domain.BookRepository;
 import com.switchfully.digibooky.book.service.dto.BookDto;
+import com.switchfully.digibooky.member.domain.Address;
+import com.switchfully.digibooky.member.domain.Member;
+import com.switchfully.digibooky.member.domain.MemberRepository;
+import com.switchfully.digibooky.member.domain.Role;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -10,9 +15,12 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.annotation.DirtiesContext;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.UUID;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 @DirtiesContext(classMode = DirtiesContext.ClassMode.AFTER_EACH_TEST_METHOD)
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
@@ -25,6 +33,7 @@ class BookControllerTest {
     @BeforeEach
     void setup(){
         BookDto book1 = new BookDto("isbn1", "title1", new Author("first1", "last1"), "1");
+        bookToRetrieve = book1;
         BookDto book2 = new BookDto("isbn2", "title2", new Author("first2", "last2"), "1");
         BookDto book3 = new BookDto("isbn3", "title3", new Author("first3", "last3"), "1");
 
@@ -73,11 +82,9 @@ class BookControllerTest {
     }
 
     @Test
-    void getByIsbn_givenANonExistingIsbn_thenThrowANoBookByIsbnException() {
+    void getByIsbn_givenANonExistingIsbn_thenReturnAnEmptyList() {
         //Then
-        Assertions.assertThatRuntimeException()
-                .isThrownBy(() -> bookController.getByIsbn("notExisting"))
-                .withMessage("There is no book with the requested Isbn");
+        assertTrue(bookController.getByIsbn("notExisting").isEmpty());
     }
 
     @Test
@@ -93,33 +100,26 @@ class BookControllerTest {
     }
 
     @Test
-    void getByTitle_givenANonExistingIsbn_thenThrowANoBookByTitleException() {
+    void getByTitle_givenANonExistingIsbn_thenReturnEmptyList() {
 
         //Then
-        Assertions.assertThatRuntimeException()
-                .isThrownBy(() -> bookController.getByTitle("notExisting"))
-                .withMessage("There is no book with the requested title");
+        assertTrue(bookController.getByTitle("notExisting").isEmpty());
     }
 
     @Test
     void getByAuthor_givenARepositoryWithBooks_thenRetrieveTheBookWithTheGivenAuthor() {
-        //Given
-        BookDto book1 = new BookDto("isbn1", "title1", new Author("first1", "last1"), "1");
-
         //When
-        List<BookDto> listOfBooks = bookController.getByAuthor(new AuthorDto("first1", "last1"));
+        List<BookDto> listOfBooks = bookController.getByAuthor(authorId);
 
         //Then
-        assertThat(listOfBooks).containsExactly(book1);
+        assertThat(listOfBooks).containsExactly(bookToRetrieve);
     }
 
     @Test
-    void getByAuthor_givenANonExistingAuthor_thenThrowANoBookByAuthorException() {
+    void getByAuthor_givenANonExistingAuthor_thenReturnEmptyList() {
 
         //Then
-        Assertions.assertThatRuntimeException()
-                .isThrownBy(() -> bookController.getByAuthor(new AuthorDto("firstUnknown", "lastUnknown")))
-                .withMessage("There is no book with the requested author");
+        assertTrue(bookController.getByAuthor("45").isEmpty());
     }
 
     @Test
@@ -129,5 +129,41 @@ class BookControllerTest {
         Assertions.assertThatRuntimeException()
                 .isThrownBy(() -> bookController.getByAuthor(null))
                 .withMessage("There is no book with the requested author");
+    }
+
+    @Test
+    void deleteByTitle_givenATitleWhoDoesNotExist_thenReturnAnEmptyList()
+    {
+        //Given
+        Book bookToDelete = new Book("isbnToDelete", "titleToDelete", new Author("firstToDelete", "lastToDelete"), "summary");
+        Book bookToDelete1 = new Book("isbnToDelete1", "titleToDelete1", new Author("firstToDelete1", "lastToDelete1"), "summary");
+        bookRepository.save(bookToDelete);
+        bookRepository.save(bookToDelete1);
+
+        //When
+        List<Book> booksToDelete = new ArrayList<>(List.of(bookToDelete, bookToDelete1));
+
+        //Then
+        List<Book> deletedBooks = bookRepository.delete(booksToDelete);
+        assertThat(deletedBooks).containsExactlyInAnyOrder(bookToDelete1, bookToDelete);
+        assertTrue(bookRepository.getByIsbn(bookToDelete.getIsbn()).isEmpty());
+    }
+
+    @Test
+    void delete_givenAListOfBookToDelete_thenReturnEmptyListIfSearchForOneOfTheBooksAfterwards()
+    {
+        //Given
+        Book bookToDelete = new Book("isbnToDelete", "titleToDelete", new Author("firstToDelete", "lastToDelete"), "summary");
+        Book bookToDelete1 = new Book("isbnToDelete1", "titleToDelete1", new Author("firstToDelete1", "lastToDelete1"), "summary");
+        bookRepository.save(bookToDelete);
+        bookRepository.save(bookToDelete1);
+
+        //When
+        List<Book> booksToDelete = new ArrayList<>(List.of(bookToDelete, bookToDelete1));
+
+        //Then
+        List<Book> deletedBooks = bookRepository.delete(booksToDelete);
+        assertThat(deletedBooks).containsExactlyInAnyOrder(bookToDelete1, bookToDelete);
+        assertTrue(bookRepository.getByIsbn(bookToDelete.getIsbn()).isEmpty());
     }
 }
